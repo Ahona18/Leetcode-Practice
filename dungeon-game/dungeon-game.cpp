c545594972a5ad2fd8866bda12575534f62bc0e7
Ahona Saha <58736467+Ahona18@users.noreply.github.com>
2021-10-02 16:14:47 +0530

    Time: 14 ms (13.57%), Space: 9 MB (14.83%) - LeetHub

--- dungeon-game/dungeon-game.cpp
+++ dungeon-game/dungeon-game.cpp
@@ -5,7 +5,7 @@ public:
         int m=dungeon[0].size();
         if(n==0)
             return 0;
-        vector<vector<int>>dp(n,vector<int> (m,0));
+        vector<vector<int>>dp(n,vector<int> (m));
         dp[n-1][m-1]=dungeon[n - 1][m - 1] > 0 ? 1 : 1 - dungeon[n - 1][m - 1];
         for(int i=n-2;i>=0;i--)
             dp[i][m-1]=max(1,dp[i+1][m-1]-dungeon[i][m-1]);
